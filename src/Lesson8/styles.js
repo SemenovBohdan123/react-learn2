2cbd34944707d1d599efbb7ef9ac899498f0eed1
SemenovBohdan123 <57286495+SemenovBohdan123@users.noreply.github.com>
2021-11-04 20:10:11 +0200

    [FEAT]: adds lesson 8 task 16 (#1)
    [FEAT]: adds lesson 8 task 16 (#1)

--- src/Lesson8/styles.js
+++ src/Lesson8/styles.js
@@ -1,22 +1,48 @@
-import { QuestionAnswerTwoTone } from "@mui/icons-material";
-import { border, borderRadius } from "@mui/system";
 import { createUseStyles } from "react-jss";
 
 
 const useStyles = createUseStyles({
   root: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     position: 'absolute',
     left: 0,
     right: 0,
     margin: 'auto',
-    top: '40%',
-    width: 320,
-    height: 120,
+    width: 720,
+    height: 300,
     padding: 10,
     backgroundColor: 'silver',
     textAlign: 'center',
     borderRadius: 15,
+    bottom: '35%'
+  },
+
+  test: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    margin: 'auto',
+    top: 0,
+  },
+
+  button: {
+    position: 'absolute',
+    left: 285,
+    right: 0,
+    margin: 'auto',
+    bottom: 0,
+  },
+
+  good: {
+    background: 'green'
+  },
+
+  bad: {
+    background: 'red'
   }
-})
+}) // TODO выровнять тесты по разным сторонам
+
 
 export default useStyles;
